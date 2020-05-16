cf13260472f4e441dbd060abaa2cec9abd905503
Gerson Azevedo <gersonazgo@gmail.com>
2020-05-16 16:11:50 -0300

    Update pubmed script

--- pubmed.rb
+++ pubmed.rb
@@ -2,10 +2,10 @@ require_relative 'lib/gscrapper'
 
 first_page = 1
 last_page = 2
-search_url = "https://pubmed.ncbi.nlm.nih.gov/?term=covid&filter=species.humans&size=200"
+crawl_url = 'https://pubmed.ncbi.nlm.nih.gov/?term=covid&filter=species.humans&size=200'
 
 (first_page..last_page).each do |page|
-  @gscrapper.visit "#{search_url}&page=#{page}"
+  @gscrapper.visit "#{crawl_url}&page=#{page}"
   @gscrapper.click_link_or_button 'Save'
   @gscrapper.select 'CSV', from: 'Format'
   @gscrapper.click_link_or_button 'Create file'
